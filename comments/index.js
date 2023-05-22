eff4dd0cba44cf8352bd08bd03e7c90d01cf21c6
Komal Chothani <komal.chothani@cybus.io>
2023-05-22 15:14:15 +0200

    Created the services for the Posts & comments. Also verified it through the postman

---
+++ comments/index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
+
+const app = express()
+app.use(bodyParser.json())
+
+/**
+ * For now we store commentsByPostId in "in-memory". So, on app restart the previously saved comments will be gone
+ * 
+ * commentByPostId: 
+       {
+            ['post-id1']: 
+                [
+                    { 
+                        id: 'comment-id1,
+                        content: 'great post,
+                    },
+                      { 
+                        id: 'comment-id2,
+                        content: 'neat post,
+                    }
+                ],
+            ['post-id2']: 
+                [
+                    { 
+                        id: 'comment-id3,
+                        content: 'Informative post,
+                    },
+                      { 
+                        id: 'comment-id4,
+                        content: 'Good post,
+                    }
+                ]  
+        }
+*/
+const commentsByPostId = {}
+
+app.get('/posts/:id/comments', (req, res) => {
+   res.send(commentsByPostId[req.params.id] || [])
+})
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex')
+    const { content } = req.body
+
+    /**
+     * Verify that post id is already present in commentsByPostId. if yes return comments array else return [] array
+     */
+    const comments = commentsByPostId[req.params.id] || []
+
+    // Push the comments
+    comments.push({ id: commentId, content })
+
+    commentsByPostId[req.params.id] = comments
+
+    res.status(201).send(comments)
+})
+
+app.listen(4001, () => {
+    console.log('Listening on 4001')
+})
