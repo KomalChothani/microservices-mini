494cb96aa7e922b2b116094582a653d7975685ba
Komal Chothani <komal.chothani@cybus.io>
2023-05-22 15:14:15 +0200

    Created the services for the Posts & comments. Also verified it through the postman

---
+++ posts/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+
+const bodyParser = require('body-parser')
+
+// Generate random id
+const { randomBytes } = require('crypto')
+
+const app = express()
+
+/**
+ * bodyParser.json() is a middleware provided by the body-parser package. It parses the JSON data in the request body and attaches it to the req.body property.
+ */
+app.use(bodyParser.json());
+
+/** 
+ * For now we store posts in "in-memory". So, on app restart the previously saved post will be gone
+*/
+const posts = {}
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+
+    const { title } = req.body;
+
+    console.log('new title', title)
+    
+    // Store information (title) into the posts
+    posts[id] = {
+        id, title
+    }
+
+    res.status(201).send(posts[id])
+})
+
+/** Express application listen on port 4000 */
+app.listen(4000, () => {
+    console.log('Listening on 4000')
+})
