7f110f2a31d248c2ccc29927fc3e82c87ddd8d6d
Komal Chothani <komal.chothani@cybus.io>
2023-05-23 13:58:32 +0200

    handle the Fetching & creating  the comments & posts through the UI

---
+++ client/src/CommentList.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+
+export default ({ postId }) => {
+    const [comments, setComments] = useState([])
+
+    const fetchComments = async () => {
+        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
+
+        setComments(res.data)
+    }
+
+    // Fetch the comments on component did mount
+    useEffect(() => {
+        fetchComments()
+    }, [])
+
+    return (
+        <div>
+            <ul>
+                {comments.map((comment) => <li key={comment.id}>{comment.content}</li>)}
+            </ul>
+        </div>
+    )
+}
--- posts/index.js
+++ posts/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 
 // Generate random id
 const { randomBytes } = require('crypto')
+const cors = require('cors')
 
 const app = express()
 
@@ -11,6 +12,12 @@ const app = express()
  * bodyParser.json() is a middleware provided by the body-parser package. It parses the JSON data in the request body and attaches it to the req.body property.
  */
 app.use(bodyParser.json());
+
+/**
+ * If we are not adding cors here, then the client (runs on port 3000) which request server (runs on port 4000)
+ * Then the request becomes failed because of the cors issue  
+ */
+app.use(cors())
 
 /** 
  * For now we store posts in "in-memory". So, on app restart the previously saved post will be gone
@@ -26,8 +33,6 @@ app.post('/posts', (req, res) => {
 
     const { title } = req.body;
 
-    console.log('new title', title)
-    
     // Store information (title) into the posts
     posts[id] = {
         id, title
